c6d447deadd2049b2246dc85c6280c8f576e7530
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-12-19 11:54:13 -0800

    Need to implement a circular buffer

--- lib/Spi/test/SpiApi/src/Test_SpiApi.cpp
+++ lib/Spi/test/SpiApi/src/Test_SpiApi.cpp
@@ -42,5 +42,22 @@ TEST(SpiApi, TEST_SINGLE_SEND_CALLS_SINGLE_TRANSMIT_COMMAND)
 
   SpiApi_Service();
   LONGS_EQUAL(SPIAPI_CMD_IN_QUEUE, result)
+  SpiApi_Service(); //Won't cause a second transmit
+}
+
+IGNORE_TEST(SpiApi, TEST_TWO_SENDS_CALL_TWO_TRANSMIT_COMMAND)
+{
+  mock().expectOneCall("Spi_Transmit")
+        .withParameter("slave", slave)
+        .withParameter("data", data);
+  mock().expectOneCall("Spi_Transmit")
+        .withParameter("slave", slave)
+        .withParameter("data", data+1);
+
+  SpiApi_Send(slave, data);
+  result = SpiApi_Send(slave, data+1);
+  LONGS_EQUAL(SPIAPI_CMD_IN_QUEUE, result)
   SpiApi_Service();
+  SpiApi_Service();
+  SpiApi_Service(); //Won't cause a third transmit
 }
